089cd91acaeb931c71d2355ae4f3c1db187b3e42
anjohan <anjohan@uio.no>
2019-08-08 15:10:25 +0200

    some reorganisation

--- reader.f90
+++ reader.f90
@@ -30,9 +30,13 @@ module mod_lammps_reader
             procedure :: open_file_explicit
             procedure :: open_file_asterisk
             generic :: open_file => open_file_asterisk, open_file_explicit
-            procedure :: read_next_header
+
             procedure :: read_step
-            procedure :: reallocate_values
+            procedure :: sort_by_property
+            procedure :: write_to_file
+
+            procedure, private :: read_next_header
+            procedure, private :: reallocate_values
             final :: close_reader
     end type
 
@@ -68,6 +72,7 @@ module mod_lammps_reader
         end function
 
         subroutine open_file_explicit(self, filename)
+            !! open a new file for reading
             class(lammps_reader), intent(inout) :: self
             character(len=*), intent(in) :: filename
 
@@ -89,31 +94,13 @@ module mod_lammps_reader
         end subroutine
 
         subroutine open_file_asterisk(self, filename, step)
+            !! read a file on the form mydump.*.bin, replacing * with step
+
             class(lammps_reader), intent(inout) :: self
             character(len=*), intent(in) :: filename
             integer, intent(in) :: step
 
-            character(len=:), allocatable :: new_filename, prefix, suffix
-            integer :: new_length, asterisk_location
-
-            asterisk_location = index(filename, "*")
-
-            prefix = filename(1:asterisk_location-1)
-            suffix = filename(asterisk_location+1:)
-
-            if (step == 0) then
-                new_length = 1
-            else
-                new_length = floor(log10(1.0d0*step)) + 1
-            end if
-
-            new_length = new_length + len(prefix) + len(suffix)
-
-            allocate(character(len=new_length) :: new_filename)
-
-            write(new_filename, '(a,i0,a)') prefix, step, suffix
-
-            call self%open_file(new_filename)
+            call self%open_file(replace_asterisk_with_step(filename, step))
         end subroutine
 
         subroutine read_step(self, success)
@@ -152,6 +139,49 @@ module mod_lammps_reader
             self%has_next_step = .false.
         end subroutine
 
+        subroutine sort_by_property(self, property_index)
+            !! sort atoms
+            !! example: if you use `dump ... id type x y z` and want to
+            !! sort the atoms by their IDs, use `property_index=1`
+
+            class(lammps_reader), intent(inout) :: self
+            integer, intent(in) :: property_index
+
+            integer :: n
+
+            n = size(self%values, 2)
+
+            block
+                integer :: indices(n), i
+
+                indices = [(i, i = 1, n)]
+
+                call argsort(self%values(property_index, :), indices)
+
+                self%values = self%values(:, indices)
+            end block
+
+        end subroutine
+
+        subroutine write_to_file(self, outunit)
+            !! write current step to file using same binary format
+            !! (but now with only one chunk)
+
+            class(lammps_reader), intent(inout) :: self
+            integer, intent(in) :: outunit
+
+            associate(hdr => self%header)
+                write(outunit) hdr%step, hdr%num_atoms, hdr%triclinic, &
+                                             hdr%boundary_conditions, hdr%boundary
+
+                if (hdr%is_triclinic) then
+                    write(outunit) hdr%angles
+                end if
+
+                write(outunit) hdr%num_columns, 1, hdr%num_columns*hdr%num_atoms, self%values
+            end associate
+        end subroutine
+
         subroutine read_next_header(self)
             class(lammps_reader), intent(inout) :: self
             self%has_next_step = .not. self%next_header%read_metadata(self%funit)
@@ -183,4 +213,31 @@ module mod_lammps_reader
 
             if (allocated(self%values)) deallocate(self%values)
         end subroutine
+
+        function replace_asterisk_with_step(filename, step) result(new_filename)
+            character(len=*), intent(in) :: filename
+            integer, intent(in) :: step
+            character(len=:), allocatable :: new_filename
+
+            character(len=:), allocatable :: prefix, suffix
+            integer :: new_length, asterisk_location
+
+            asterisk_location = index(filename, "*")
+
+            prefix = filename(1:asterisk_location-1)
+            suffix = filename(asterisk_location+1:)
+
+            if (step == 0) then
+                new_length = 1
+            else
+                new_length = floor(log10(1.0d0*step)) + 1
+            end if
+
+            new_length = new_length + len(prefix) + len(suffix)
+
+            allocate(character(len=new_length) :: new_filename)
+
+            write(new_filename, '(a,i0,a)') prefix, step, suffix
+
+        end function
 end module
